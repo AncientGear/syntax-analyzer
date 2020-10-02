df145da6b83f21d925590887a7cf35f2dadb9a69
angelruiz2101 <ruzangel497@gmail.com>
2020-10-02 16:36:32 +0000

    example
    Signed-off-by: angelruiz2101 <ruzangel497@gmail.com>

--- src/app/services/semantic-errors.service.ts
+++ src/app/services/semantic-errors.service.ts
@@ -1,9 +1,65 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class SemanticErrorsService {
 
-  constructor() { }
+	varGlobal = [];
+	varLocal = [];
+	errsem = [];
+	isLocal:boolean;
+	tokens = [{
+		line : 1,
+		lexeme : 'a',
+		token: 'id',
+		message: '',
+		dataType : 'number',
+		context : 'global'
+	},
+	{
+		line : 2,
+		lexeme : 'b',
+		token: 'id',
+		message: '',
+		dataType : '',
+		context : ''
+	},
+	{
+		line : 3,
+		lexeme : 'c',
+		token: 'id',
+		message: '',
+		dataType : 'string',
+		context : ''
+	},
+	{
+		line : 4,
+		lexeme : 'a',
+		token: 'id',
+		message: '',
+		dataType : '',
+		context : ''
+	}]
+	
+	arimeticOperators: {
+	id: 'OA',
+	options: ['+', '-', '/', '*'],
+	counter: 0
+	};
+	relationalsOperators: {
+	id: 'OR',
+	options: ['<', '<=', '>', '>=', '!=', '=='],
+	counter: 0
+	};
+
+	constructor() { 
+		this.isLocal = false;
+		this.analizaTable();
+	}
+	analizaTable() {
+		this.tokens.forEach(toke => {
+			
+		});
+	}
 }
