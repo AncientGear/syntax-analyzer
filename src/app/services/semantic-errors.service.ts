7b14b94094f56727992f4eb0f2c8bbd3e34697fa
angelruiz2101 <ruzangel497@gmail.com>
2020-10-03 01:21:58 +0000

    changes RM
    Signed-off-by: angelruiz2101 <ruzangel497@gmail.com>

--- src/app/services/semantic-errors.service.ts
+++ src/app/services/semantic-errors.service.ts
@@ -1,3 +1,4 @@
+import { NULL_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,61 +6,90 @@ import { Injectable } from '@angular/core';
 })
 export class SemanticErrorsService {
 
-	varGlobal = [];
-	varLocal = [];
 	errsem = [];
-	isLocal:boolean;
-	tokens = [{
-		line : 1,
-		lexeme : 'a',
-		token: 'id',
-		message: '',
-		dataType : 'number',
-		context : 'global'
-	},
-	{
-		line : 2,
-		lexeme : 'b',
-		token: 'id',
-		message: '',
-		dataType : '',
-		context : ''
-	},
-	{
-		line : 3,
-		lexeme : 'c',
-		token: 'id',
-		message: '',
-		dataType : 'string',
-		context : ''
-	},
-	{
-		line : 4,
-		lexeme : 'a',
-		token: 'id',
-		message: '',
-		dataType : '',
-		context : ''
-	}]
-	
-	arimeticOperators: {
-	id: 'OA',
-	options: ['+', '-', '/', '*'],
-	counter: 0
-	};
-	relationalsOperators: {
-	id: 'OR',
-	options: ['<', '<=', '>', '>=', '!=', '=='],
-	counter: 0
-	};
+	numErr: number;
+	islocal;
 
 	constructor() { 
-		this.isLocal = false;
-		this.analizaTable();
+		this.islocal = false;
+		this.numErr = 0;
 	}
-	analizaTable() {
-		this.tokens.forEach(toke => {
-			
-		});
+
+	analizaTable(tokens) {
+		for (let i = 0; i < tokens.length; i++){
+			const element = tokens[i];
+			switch (element) {
+				case element.token == 'OA' || element.token == 'OR':
+					this.compare(tokens[i-1], element,  tokens[i+1]);
+					break;
+				case element.token == 'DEL':
+					switch (element.lexeme) {
+						case '{':
+							this.islocal = true;
+							break;
+						case '}':
+							this.islocal = false;
+						default:
+							break;
+					}
+					break;
+				case element.token == 'ID':
+						if(element.dataType = undefined){
+							this.numErr++;
+							this.errsem.push({
+								message : 'ERRSEM'.concat(this.numErr.toString()),
+								lexeme :  element.lexeme,
+								line : element.line,
+								description : 'Indefinida la variable'
+							});
+						}
+					break;
+			}	
+		}
+	}
+	
+	compare(ante, ahora,siguiente){
+		if(ante == undefined || siguiente == undefined){
+			return 0;
+		}
+		else if(ante.dataType == undefined || siguiente.dataType == undefined){
+			return 0;
+		}
+		else{
+			if(ante.dataType == siguiente.dataType){
+				return 1;
+			}
+			else if(this.islocal == false && (ante.context == 'local' || siguiente.context == 'local')){
+				if(ante.context == 'local'){
+					this.numErr++;
+					this.errsem.push({
+						message : 'ERRSEM'.concat(this.numErr.toString()),
+						lexeme :  ante.lexeme,
+						line : ante.line,
+						description : 'Indefinida la variable'
+					});
+					return 0;
+				}
+				else if(siguiente.context == 'local'){
+					this.numErr++;
+					this.errsem.push({
+						message : 'ERRSEM'.concat(this.numErr.toString()),
+						lexeme :  siguiente.lexeme,
+						line : siguiente.line,
+						description : 'Indefinida la variable'
+					});
+					return 0;
+				}
+			}
+			else{
+				this.numErr++;
+				this.errsem.push({
+					message : 'ERRSEM'.concat(this.numErr.toString()),
+					lexeme :  ante.lexeme + ahora.lexeme + siguiente.lexeme,
+					line : ahora.line,
+					description : 'Incompatibilidad de tipos'
+				});
+			}
+		}
 	}
 }
