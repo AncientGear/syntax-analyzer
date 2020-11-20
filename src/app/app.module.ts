a2eaeeb1bf96b1caf7a8886d30bfde2d5b14d92b
Alejandro <ruzangel497@gmail.com>
2020-11-20 00:10:53 -0600

    cambios ruiz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { InputCodeComponent } from './components/input-code/input-code.component
 import { TokensTableComponent } from './components/tokens-table/tokens-table.component';
 import { ErrorsTableComponent } from './components/errors-table/errors-table.component';
 import { DownloadTxtComponent } from './components/download-txt/download-txt.component';
-import { TriploComponent } from './triplo/triplo.component';
+import { TriploComponent } from './components/triplo/triplo.component';
 import { TriploService } from './services/triplo/triplo.service';
 import { HttpClientModule } from '@angular/common/http';
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  urlAPI: 'http://localhost:3000'
+  urlAPI: 'https://triplo-api.herokuapp.com'
 };
